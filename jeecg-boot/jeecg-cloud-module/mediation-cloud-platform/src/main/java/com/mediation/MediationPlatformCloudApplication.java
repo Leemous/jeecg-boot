8a8620100073e1889e6a2605d6c445d5f60320d6
leemous-mac <leemous@163.com>
2020-12-23 20:13:32 +0800

    移除无用import

--- jeecg-boot/jeecg-cloud-module/mediation-cloud-platform/src/main/java/com/mediation/MediationPlatformCloudApplication.java
+++ jeecg-boot/jeecg-cloud-module/mediation-cloud-platform/src/main/java/com/mediation/MediationPlatformCloudApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * 微服务启动入口
